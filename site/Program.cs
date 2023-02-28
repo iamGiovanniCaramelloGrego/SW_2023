b75c5e6e90b1f4bb5072fdb0ab217bed830c2d5a
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-02-28 12:18:20 -0300

    Add files via upload

---
+++ site/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ex_aviao;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("AVIÃO POO");
+        Aviao aviao1 = new Aviao();
+        
+        aviao1.modelo = "malaysia 3030";
+        aviao1.marca = "branco";
+        
+        aviao1.exibir();
+        aviao1.acelerar();
+        aviao1.exibir();
+        aviao1.acelerar();
+        aviao1.exibir();
+    }
+}
