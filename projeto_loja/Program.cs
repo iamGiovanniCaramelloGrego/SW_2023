c87d2159bc45341cca7e9b91c30e0ace86a8e12c
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-03-28 12:29:12 -0300

    loja 28/03

---
+++ projeto_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //produto p1 = new Produto();
+        Produto p1 = new Produto("xyz");
+        p1.MostraDados();
+        
+    }
+}
