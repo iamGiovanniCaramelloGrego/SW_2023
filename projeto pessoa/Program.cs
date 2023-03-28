d36fc15b652dedaa4a296aecdd4668dcaea55467
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-03-28 12:28:26 -0300

    pessoa 28/03

---
+++ projeto pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p1 = new Pessoa("godofredo",18);
+        //p1.AlteraNome("GIOVANNI");
+        //p1.AlteraSobreNome("CARAMELLO");
+        //p1.AlteraIdade(16);
+        p1.MostraDados();
+    }
+}
