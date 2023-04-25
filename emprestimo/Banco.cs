2740fa4fff4edd6326bc411ac7d79e9250a7d43c
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-04-25 11:40:30 -0300

    25/04

---
+++ emprestimo/Banco.cs
@@ -0,0 +1,50 @@
+﻿namespace emprestimo;
+class Banco
+{
+   //Funções inicias do banco
+
+    public int Numero {get; set;}
+    private double Saldo { get;  set;}
+    public double Limite {get; private set;}
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+    public void Depositar( double valor){
+        this.Saldo += valor;
+    }
+    public void Sacar(double valor){
+        if (this.Saldo + this.Limite <= valor){
+            Console.WriteLine("você esta sacando mais do que você tem");
+        }else{
+             this.Saldo -= valor;
+        }
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+    // empréstimo
+
+    public double parcela {get; set;}
+    public double valor {get; set;}
+
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
+        
+        double div = dinheiro/parcelas;
+        double mult = dinheiro * 0.06;
+        double soma = mult + div;
+        double total = soma *parcelas;
+
+        string resultado = "valor da parcela sem juros é: "+ div +". valor da parcela com juros: "+ mult + ". valor do juros com prestações é de: "+ soma +"e o valor total é de: "+total;
+        return resultado;
+        
+     
+    }
+    public void imprimi(string texto){
+        Console.WriteLine(texto);
+    } 
+}
+    
---
+++ emprestimo/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace emprestimo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco Daora");
+        Banco b = new Banco();
+        //c.Saldo = 1000;
+        b.Depositar(1000);
+        b.Sacar(1600);
+        b.AjustarLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+        
+
+        Console.WriteLine("seu saldo total é de " + valortotal);
+
+        b.imprimi(b.emprestimo(12,1000));
+    }
+}
+
